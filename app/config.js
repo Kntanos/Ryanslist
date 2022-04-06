c444d88f80acccc972801180bb278a54637b52c0
Kostas Ntanos <kostas@ntanos.co.uk>
2022-04-06 20:06:03 +0100

    Add start page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Create and search classifieds on Ryanslist',
 
   // Default port that prototype runs on
   port: '3000',
