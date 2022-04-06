6458bdd897244cba796ce1a492133dc0d8168f56
Kostas Ntanos <kostas@ntanos.co.uk>
2022-04-06 20:21:03 +0100

    Add route for branch for create or search

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post('/juggling-balls-answer', function (req, res) {
+
+  var createOrSearch = req.session.data['create-search']
+  
+  if (createOrSearch == "create"){
+    res.redirect('/create-new')
+  } else {
+    res.redirect('/search-new')
+  }
+
+})
 
 module.exports = router
